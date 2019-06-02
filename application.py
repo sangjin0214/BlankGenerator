0b989ba5d1d5d56a027d829ddaad20056517219c
SangJinEom <sangjineom0214@gmail.com>
2019-06-02 05:49:17 +0000

    first commit

---
+++ application.py
@@ -0,0 +1,72 @@
+from flask import Flask, request
+import sys
+import blankGenerator
+application = Flask(__name__)
+
+
+@application.route("/")
+def test():
+    test, answer = blankGenerator.process()
+    exp = ['!', '@', '#', '$', '.', ',', '-', '&', '(', ')', '*', '?', ';', ':', '"', "'"]
+    test_form = ''
+    test_form += '<p>문제</p><br>'
+    test_form += '<form action="/answer" method="post">'
+    num = 0
+    n = 1
+    for text in test:
+        test_form += str(n) + '번 지문<br>'
+        for word in text:
+            if word == '':
+                test_form += ' <input type="text" name="test' + str(num) + '">'
+                num += 1
+            else:
+                if word in exp:
+                    test_form += word
+                else:
+                    test_form += ' ' + word
+        n += 1
+        test_form += '<br><br><br>'
+    test_form += '<input type="hidden" name="num" value="' + str(num-1) + '"><br>'
+    num = 0
+    for temp in answer:
+        test_form += '<input type="hidden" name="n' + str(num) + '" value="' + temp + '">'
+        num += 1
+    test_form += '<input type="hidden" name="n" value="' + str(num-1) + '">'
+    test_form += '<input type="submit" value="제출">'
+    test_form += '</form>'
+    return test_form
+
+
+@application.route("/answer", methods=['POST'])
+def answer():
+    test_form = ''
+    test_form += '<p>결과</p><br><br>'
+    num = request.form['num']
+    n = request.form['n']
+    ans = []
+    answer = []
+    i = 0
+    while True:
+        answer.append(request.form['n' + str(i)])
+        if i == int(n):
+            break
+        i += 1
+    i = 0
+    while True:
+        ans.append(request.form['test' + str(i)])
+        if i == int(num):
+            break
+        i += 1
+    n = 0
+    m = ''
+    for m in answer:
+        if m != ans[n]:
+            test_form += str(n+1) + '. ' + ans[n] + ' -> ' + m + '<br>'
+        else:
+            test_form += str(n+1) + '. Correct<br>'
+        n += 1
+    return test_form
+
+
+if __name__ == "__main__":
+    application.run(host='0.0.0.0', port=int(sys.argv[1]))
---
+++ blankGenerator.py
@@ -0,0 +1,51 @@
+from random import randint
+
+
+def process():
+    n = 0
+    temp = ""
+    text = []
+    exp = ['!', '@', '#', '$', '.', ',', '-', '&', '(', ')', '*', '?', ';', ':', '"', "'"]
+    answer = []
+    f = open("./questions/B.txt", "r")
+    for line in f.readlines():
+        for e in exp:
+            line = line.replace(e, ' ' + e + ' ') if e in line else line
+        line = line.replace('\xad', '-')
+        if n != 0:
+            temp += line
+        n += 1
+        if line == "\n":
+            text.append(temp)
+            temp = ""
+            n = 0
+    splitted_txt = []
+    splitted_text = []
+    for txt in text:
+        for word in txt.split():
+            splitted_txt.append(word)
+        splitted_text.append(splitted_txt)
+        splitted_txt = []
+    q = 0
+    for splitted_txt in splitted_text:
+        for word in splitted_txt:
+            if word == '':
+                q += 1
+    print('빈칸 개수 : ' + str(q))
+    n = 0
+    m = 0
+    for splitted_txt in splitted_text:
+        for word in splitted_txt:
+            if randint(1, 100) <= 30 and word not in exp:
+                answer.append(word)
+                splitted_text[m][n] = ''
+            n += 1
+        n = 0
+        m += 1
+    q = 0
+    for splitted_txt in splitted_text:
+        for word in splitted_txt:
+            if word == '':
+                q += 1
+    print('빈칸 개수 : ' + str(q))
+    return splitted_text, answer
